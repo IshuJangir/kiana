4bd814d630f5679d50566e1b1339d6d92dd896c4
IshuJangir <ishujangir0000@gmail.com>
2024-06-30 19:27:38 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import Team from './components/ui/Team'
-import Contact from './components/ui/Contact'
+import Team from './components/ui/Team';
+import Contact from './components/ui/Contact';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/kiana">
     <Routes>
       <Route path="/" element={<App />} />
-      <Route path="team" element={< Team/>} />
+      <Route path="team" element={<Team />} />
       <Route path="contact" element={<Contact />} />
     </Routes>
   </BrowserRouter>
